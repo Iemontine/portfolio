0c7283dc0d81c0a699f6b397a0829611975f096c
lemontine <computereality@gmail.com>
2024-10-01 18:33:21 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ContentBox from './components/ContentBox';
+import ArtBox from './components/ArtBox';
+import { PAGE_CONTENT, BACKGROUND_COLOR, INTERFACE_COLOR } from './constants';
 
-function App() {
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: BACKGROUND_COLOR, color: INTERFACE_COLOR }} className="h-screen w-screen text-xl px-8 py-8 grid grid-cols-[auto,1fr,1fr] grid-rows-[1fr,1fr] gap-8">
+      <div className="row-span-2 flex flex-col justify-center items-center space-y-2">
+        {PAGE_CONTENT.map((page, index) => (
+          <div
+            key={page.id}
+            style={{ borderColor: INTERFACE_COLOR, backgroundColor: index === currentPage ? INTERFACE_COLOR : 'transparent' }}
+            className="h-12 w-4 border transition-all duration-300 ease-in-out"
+          />
+        ))}
+      </div>
+
+      <ContentBox currentPage={currentPage} setCurrentPage={setCurrentPage} />
+
+      {/* LEMONTINE box */}
+      <div style={{ borderColor: INTERFACE_COLOR }} className="relative border p-4 z-10">
+        <div className="relative z-20">
+          <span className="text-2xl">LEMONTINE</span>
+          <p>some simple about me textsome simple about me textsome simple about me textsome simple about me textsome simple about me textsome simple about me textsome simple about me textsome simple about me text</p>
+        </div>
+        {/* Absolute-positioned boxes (overlapping the LEMONTINE box) */}
+        <div className="absolute right-0 top-0 flex space-x-4 z-10">
+            <div style={{ borderColor: INTERFACE_COLOR, backgroundColor: BACKGROUND_COLOR, transform: 'translate(-10px, -10px)' }} className="border p-2 h-32 w-64">
+            <p>will be filled with animated/panning text eventually, or images</p>
+            </div>
+            <div style={{ borderColor: INTERFACE_COLOR, backgroundColor: BACKGROUND_COLOR, transform: 'translate(-10px, -10px)' }} className="h-32 w-16 border"></div>
+            <div style={{ borderColor: INTERFACE_COLOR, backgroundColor: BACKGROUND_COLOR, transform: 'translate(-10px, -10px)' }} className="h-32 w-40 border"></div>
+        </div>
+      </div>
+
+      {/* Absolute-positioned boxes (overlapping the LEMONTINE box) */}
+
+
+      <ArtBox currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      {/* <div style={{ borderColor: INTERFACE_COLOR }} className="border p-4">
+        <p>ascii art to go here</p>
+      </div> */}
     </div>
   );
-}
+};
 
 export default App;
